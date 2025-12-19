22a90c475080734fd3f7cd0af223e2f46287a3c9
zinny <zinedinesehili@gmail.com>
2025-12-19 10:55:03 -0500

    added logger

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+
 if __name__ == "__main__":
     main()
