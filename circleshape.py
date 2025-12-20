450a106f1878a0c949f355c4fa6579d79c97cf6a
zinny <zinedinesehili@gmail.com>
2025-12-20 18:06:58 -0500

    added player collision

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        r1 = self.radius
+        r2 = other.radius
+
+        if (distance <= (r1 + r2)):
+            return True
+        return False
